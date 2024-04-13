7a1beb12f699f052854202bf09272e8fca9cd775
Eldar Ulanov <ulanov.eldar05@gmail.com>
2024-04-13 14:24:15 +0200

    java doc documentation for tcpoutputstrategy

--- src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
@@ -6,18 +6,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executors;
 
+/**
+ * Implements the OutputStrategy interface for outputting data over TCP.
+ * This class handles the creation of a TCP server socket that listens on a specified port
+ * and sends data to a connected client.
+ */
 public class TcpOutputStrategy implements OutputStrategy {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
 
+    /**
+     * Creates a TCP server that listens on the specified port and handles client connections.
+     *
+     * @param port The port number on which the server will listen for incoming connections.
+     */
     public TcpOutputStrategy(int port) {
         try {
             serverSocket = new ServerSocket(port);
             System.out.println("TCP Server started on port " + port);
 
-            // Accept clients in a new thread to not block the main thread
+            // Handle client connections in a new thread to not block the main thread.
             Executors.newSingleThreadExecutor().submit(() -> {
                 try {
                     clientSocket = serverSocket.accept();
@@ -32,6 +42,15 @@ public class TcpOutputStrategy implements OutputStrategy {
         }
     }
 
+    /**
+     * Sends data in a formatted way with TCP to a client.
+     * Data is formatted with comma seperations with various patient-identifying data types.
+     *
+     * @param patientId ID of patient
+     * @param timestamp The timestamp of data collection
+     * @param label The type of data 
+     * @param data The actual data
+     */
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         if (out != null) {
