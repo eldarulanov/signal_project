053ab7f545045c645d6e14d723f19ec64b27e962
Eldar Ulanov <ulanov.eldar05@gmail.com>
2024-04-13 14:20:31 +0200

    javaDoc documentation for patientdatagenerator

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,17 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * A data generator to simulate patient's health data.
+ * It is a interface with the different data types for health tracking
+ */
 public interface PatientDataGenerator {
+    
+    /**
+     * Generates and outputs data for a specific patient.
+     *
+     * @param patientId The ID of the patient for whom data is being generated.
+     * @param outputStrategy The output strategy to be used for dispatching generated data.
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
